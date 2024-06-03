33f802b2930f03b11074ef0fcce993965d86ee76
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2024-06-03 12:22:57 +0500

    Basic template

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	cf "farmish/config"
+	"farmish/config/logger"
+	"farmish/postgresql"
+	"path/filepath"
+	"runtime"
+)
 
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath   = filepath.Dir(b)
+)
+
+func main() {
+	config := cf.Load()
+	logger := logger.NewLogger(basepath, config.LOG_PATH)
+	em := cf.NewErrorManager(logger)
+
+	db, err := postgresql.ConnectDB(config)
+	em.CheckErr(err)
+	defer db.Close()
 }
